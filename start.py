437e54f9dbe5a4ff49d86346b2789fe76271f694
杜宇鹏 <duyupeng36@foxmail.com>
2025-10-13 22:59:49 +0800

    C++ 设置环境

--- start.py
+++ start.py
@@ -1,8 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
 import os
 
 def main():
+    os.system("gitbook install")
     os.system("gitbook build")
-    os.system("cd _book && python3t -m http.server 4000")
+    os.system("cd _book && python -m http.server 4000")
+
 
 if __name__ == "__main__":
     main()
